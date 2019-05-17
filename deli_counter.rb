a58f3434ee8d84002cd1334ebd6ad28ee18c377a
Reina Ewing <reina.castro3@gmail.com>
2019-05-17 14:39:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,6 @@ end
 end 
 
 def take_a_number(katz_deli, name)
-  if katz_deli.length == 0 
-    
-  katz_deli.push(name)
+  katz_deli << name
+  puts "#{name} your position in line #{katz_deli.length}"
 end 
