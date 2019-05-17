43b7e9dd51a8fd12576015ad4eafd311a0322be7
Reina Ewing <reina.castro3@gmail.com>
2019-05-17 14:28:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
   else 
     message = "The line is currently: "
     katz_deli.each.with_index do |name, index|
-      message += "{#{index +1}. #{name}."
+      message += "#{index +1}. #{name}."
   end 
   puts message
 end 
