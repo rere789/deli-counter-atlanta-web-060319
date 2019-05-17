d7a4799e3414cb567a242cdf676316aded9412be
Reina Ewing <reina.castro3@gmail.com>
2019-05-17 14:31:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
-    message = "The line is currently: "
+    message = "The line is currently:"
     katz_deli.each.with_index do |name, index|
-      message += "#{index + 1}. #{name}"
+      message += " #{index + 1}. #{name}"
   end 
   puts message
 end 
